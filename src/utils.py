3a18acef40c36d3cc8db01c21fa371ffc7fc4e6d
KathTheDragon <kaththedragon@gmail.com>
2022-03-31 08:54:50 +0100

    Guard against an empty list

--- src/utils.py
+++ src/utils.py
@@ -14,8 +14,8 @@ def partition(strings: list[str], separator: str) -> list[list[str]]:
 
 def strip(strings: list[str]) -> tuple[str, list[str], str]:
     leading = trailing = ''
-    if strings[0].isspace():
+    if strings and strings[0].isspace():
         leading = strings.pop(0)
-    if strings[-1].isspace():
+    if strings and strings[-1].isspace():
         trailing = strings.pop()
     return leading, strings, trailing
