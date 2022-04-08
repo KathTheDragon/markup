9d9a5ebb3eb196d3824b152ae55c6481ea63b8bf
KathTheDragon <kaththedragon@gmail.com>
2022-04-08 06:01:29 +0100

    Rework table tests

--- tests/nodes/test_table.py
+++ tests/nodes/test_table.py
@@ -48,27 +48,151 @@ class Test_table_node:
     def test_first_row_and_column_are_heading_cells_if_rows_and_cols_in_header_data_arg():
         assert table.table_node('table', data=['headers=rows,cols'], text=['foo', '|', 'bar', '/', 'baz', '|', 'bif']) == '<table><tr><th>foo</th><th>bar</th></tr><tr><th>baz</th><td>bif</td></tr></table>'
 
-    def test_left_angle_brackets_increase_colspan_of_cell_to_the_left():
-        assert table.table_node('table', text=['foo', '|', '<', '|', '<']) == '<table><tr><td colspan="3">foo</td></tr></table>'
 
-    def test_row_cannot_begin_with_left_angle_bracket():
+@staticmethods
+class Test_merge_left:
+    def test_left_arrow_cannot_be_merged_into_empty_row():
         with raises(table.MarkupError):
-            table.table_node('table', text=['<', '|', '<'])
+            table._merge_left([], ('', ['<'], ''))
 
-    def test_carets_increase_rowspan_of_cell_above():
-        assert table.table_node('table', text=['foo', '/', '^', '/', '^']) == '<table><tr><td rowspan="3">foo</td></tr></table>'
-
-    def test_column_cannot_begin_with_caret():
+    def test_left_arrow_cannot_be_merged_after_up_arrow():
+        row = table._merge_left([], ('', ['^'], ''))
         with raises(table.MarkupError):
-            table.table_node('table', text=['^', '/', '^'])
+            table._merge_left(row, ('', ['<'], ''))
 
-    def test_caret_under_a_merged_cell_continued_by_dots_and_increase_rowspan_of_cell():
-        assert table.table_node('table', text=['foo', '|', '<', '|', '<', '/', '^', '|', '.', '|', '.']) == '<table><tr><td rowspan="2" colspan="3">foo</td></tr></table>'
+    def test_left_arrow_increments_colspan_of_last_merged_cell():
+        row = [
+            {'data': ('', ['foo'], ''), 'cols': 1, 'rows': 1},
+            {'data': ('', ['bar'], ''), 'cols': 3, 'rows': 1},
+        ]
+        assert table._merge_left(row, ('', ['<'], '')) == [
+            {'data': ('', ['foo'], ''), 'cols': 1, 'rows': 1},
+            {'data': ('', ['bar'], ''), 'cols': 4, 'rows': 1},
+        ]
 
-    def test_must_have_correct_number_of_carets_and_dots_for_merged_cell():
+    def test_dot_cannot_be_merged_into_empty_row():
         with raises(table.MarkupError):
-            table.table_node('table', text=['foo', '|', '<', '|', '<', '/', '^', '|', '.', '|', 'bar'])
+            table._merge_left([], ('', ['.'], ''))
 
-    def test_caret_must_be_aligned_with_start_of_merged_cell():
+    def test_dot_cannot_be_merged_after_text():
+        row = table._merge_left([], ('', ['foo'], ''))
         with raises(table.MarkupError):
-            table.table_node('table', text=['foo', '|', '<', '|', '<', '/', 'bar', '|', '^', '|', '.'])
+            table._merge_left(row, ('', ['.'], ''))
+
+        row = table._merge_left(row, ('', ['<'], ''))
+        with raises(table.MarkupError):
+            table._merge_left(row, ('', ['.'], ''))
+
+    def test_dot_increments_colspan_of_last_merged_caret():
+        row = [
+            {'data': ('', ['^'], ''), 'cols': 1, 'rows': 1},
+            {'data': ('', ['^'], ''), 'cols': 3, 'rows': 1},
+        ]
+        assert table._merge_left(row, ('', ['.'], '')) == [
+            {'data': ('', ['^'], ''), 'cols': 1, 'rows': 1},
+            {'data': ('', ['^'], ''), 'cols': 4, 'rows': 1},
+        ]
+
+    def test_text_always_merged():
+        assert table._merge_left([], ('', ['foo'], '')) == [
+            {'data': ('', ['foo'], ''), 'cols': 1, 'rows': 1},
+        ]
+        row = [
+            {'data': ('', ['foo'], ''), 'cols': 1, 'rows': 1},
+            {'data': ('', ['bar'], ''), 'cols': 3, 'rows': 1},
+        ]
+        assert table._merge_left(row, ('', ['baz'], '')) == [
+            {'data': ('', ['foo'], ''), 'cols': 1, 'rows': 1},
+            {'data': ('', ['bar'], ''), 'cols': 3, 'rows': 1},
+            {'data': ('', ['baz'], ''), 'cols': 1, 'rows': 1},
+        ]
+
+
+def _make_row(row: str) -> list[dict]:
+    from functools import reduce
+    from markup.src.utils import partition, strip
+
+    return reduce(table._merge_left, [strip([cell]) for cell in row.split()], [])
+
+
+@staticmethods
+class Test_merge_up:
+    def test_row_with_carets_cannot_be_merged_into_empty_table():
+        row = _make_row('foo < ^ . . bar')
+        with raises(table.MarkupError):
+            table._merge_up([], row)
+
+    def test_caret_must_be_aligned_with_some_cell_in_last_row_of_table():
+        table_ = [
+            _make_row('foo < bar < < baz'),
+        ]
+
+        row = _make_row('foo ^ bar < < <')
+        with raises(table.MarkupError):
+            table._merge_up(table_, row)
+
+        row = _make_row('foo < ^ bar < <')
+        with raises(table.MarkupError):
+            table._merge_up(table_, row)
+
+        row = _make_row('foo < ^ . . bar')
+        _row_1 = _make_row('foo < bar < < baz')
+        _row_1[1]['rows'] = 2
+        assert table._merge_up(table_, row) == [
+            _row_1,
+            _make_row('foo < ^ . . bar'),
+        ]
+
+    def test_caret_increases_rowspan_of_lowest_aligned_non_caret_cell():
+        table_ = [
+            _make_row('foo < bar < < baz'),
+            _make_row('foo < bar < < baz'),
+            _make_row('foo < bar < < baz'),
+        ]
+        row = _make_row('foo < ^ . . bar')
+        _row_3 = _make_row('foo < bar < < baz')
+        _row_3[1]['rows'] = 2
+        assert table._merge_up(table_, row) == [
+            _make_row('foo < bar < < baz'),
+            _make_row('foo < bar < < baz'),
+            _row_3,
+            _make_row('foo < ^ . . bar'),
+        ]
+
+
+@staticmethods
+class Test_make_tr:
+    def test_returns_empty_string_if_row_contains_no_content_cells():
+        row = _make_row('^ . . ^ . ^ ^ ^ .')
+        assert table._make_tr(row, headers=set(), row_num=1) == ''
+
+    def test_returns_tr_tag_otherwise():
+        row = _make_row('foo bar baz')
+        assert table._make_tr(row, headers=set(), row_num=0) == '<tr><td>foo</td><td>bar</td><td>baz</td></tr>'
+
+
+@staticmethods
+class Test_make_td:
+    def test_returns_empty_list_if_cell_is_caret():
+        cell = {'data': ('', ['^'], ''), 'rows': 1, 'cols': 1}
+        assert table._make_td(cell, headers=set(), row_num=1, col_num=1) == []
+
+    def test_tag_is_th_if_row_0_and_rows_in_headers():
+        cell = {'data': ('', ['foo'], ''), 'rows': 1, 'cols': 1}
+        assert table._make_td(cell, headers={'rows'}, row_num=0, col_num=1) == ['', '<th>foo</th>', '']
+
+    def test_tag_is_th_if_col_0_and_cols_in_headers():
+        cell = {'data': ('', ['foo'], ''), 'rows': 1, 'cols': 1}
+        assert table._make_td(cell, headers={'cols'}, row_num=1, col_num=0) == ['', '<th>foo</th>', '']
+
+    def test_tag_has_rowspan_attribute_if_cell_spans_more_than_1_row():
+        cell = {'data': ('', ['foo'], ''), 'rows': 3, 'cols': 1}
+        assert table._make_td(cell, headers=set(), row_num=1, col_num=1) == ['', '<td rowspan="3">foo</td>', '']
+
+    def test_tag_has_colspan_attribute_if_cell_spans_more_than_1_row():
+        cell = {'data': ('', ['foo'], ''), 'rows': 1, 'cols': 2}
+        assert table._make_td(cell, headers=set(), row_num=1, col_num=1) == ['', '<td colspan="2">foo</td>', '']
+
+    def test_leading_and_trailing_whitespace_placed_outside_tag():
+        cell = {'data': ('\n  ', ['foo'], '\r\t'), 'rows': 1, 'cols': 1}
+        assert table._make_td(cell, headers=set(), row_num=1, col_num=1) == ['\n  ', '<td>foo</td>', '\r\t']
