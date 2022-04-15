185c1e9474791c6574ac57e23891d05d5d8449cd
KathTheDragon <kaththedragon@gmail.com>
2022-04-15 13:43:12 +0100

    Factor out Node.parse_data into its own function

--- src/nodes/base.py
+++ src/nodes/base.py
@@ -15,35 +15,39 @@ class Node:
         return html(self.tag, self.make_attributes(), self.make_content())
 
     def parse_data(self, data: list[str]) -> Attributes:
-        positionals = reversed(param for param in self.params if not param.endswith('?') and not param.endswith('='))
-        boolean = {param.removesuffix('?') for param in self.params if param.endswith('?')}
-        named = {param.removesuffix('=') for param in self.params if param.endswith('=')}
-        data_dict = {}
-        for arg in data:
-            # Test for named argument
-            if '=' in arg:
-                name, value = arg.split('=', maxsplit=1)
-                if name in named:
-                    named.remove(name)
-                    data_dict[name] = value
-                else:
-                    raise InvalidData()
-            # Test for boolean argument
-            elif arg in boolean:
-                boolean.remove(arg)
-                data_dict[arg] = True
-            # Must be positional
-            elif positionals:
-                name = positionals.pop()
-                data_dict[name] = arg
-            else:
-                raise InvalidData()
-        if positionals:
-            raise InvalidData()
-        return data_dict
+        return parse_data(data, self.params)
 
     def make_attributes(self) -> Attributes:
         return self.attributes
 
     def make_content(self) -> Optional[list[str]]:
         return self.text
+
+
+def parse_data(data: list[str], params: list[str]) -> Attributes:
+    positionals = reversed(param for param in params if not param.endswith('?') and not param.endswith('='))
+    boolean = {param.removesuffix('?') for param in params if param.endswith('?')}
+    named = {param.removesuffix('=') for param in params if param.endswith('=')}
+    data_dict = {}
+    for arg in data:
+        # Test for named argument
+        if '=' in arg:
+            name, value = arg.split('=', maxsplit=1)
+            if name in named:
+                named.remove(name)
+                data_dict[name] = value
+            else:
+                raise InvalidData()
+        # Test for boolean argument
+        elif arg in boolean:
+            boolean.remove(arg)
+            data_dict[arg] = True
+        # Must be positional
+        elif positionals:
+            name = positionals.pop()
+            data_dict[name] = arg
+        else:
+            raise InvalidData()
+    if positionals:
+        raise InvalidData()
+    return data_dict
