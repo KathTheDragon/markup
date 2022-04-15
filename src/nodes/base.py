ff98eaf3f90cc2f53db555083e7db93d5ad29339
KathTheDragon <kaththedragon@gmail.com>
2022-04-15 14:18:27 +0100

    Fix type

--- src/nodes/base.py
+++ src/nodes/base.py
@@ -24,7 +24,7 @@ class Node:
         return self.text
 
 
-def parse_data(data: list[str], params: list[str]) -> Attributes:
+def parse_data(data: list[str], params: tuple[str, ...]) -> Attributes:
     positionals = reversed(param for param in params if not param.endswith('?') and not param.endswith('='))
     boolean = {param.removesuffix('?') for param in params if param.endswith('?')}
     named = {param.removesuffix('=') for param in params if param.endswith('=')}
